254929bde4d5e97877937c625b6b80554c057273
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-06-15 16:16:23 +0100

    unuse import

--- src/main/java/com/jumong/E/TMotors/controller/AuthController.java
+++ src/main/java/com/jumong/E/TMotors/controller/AuthController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
